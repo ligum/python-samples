e7be866cf8909562f567c606419a76f41e800876
Vova <ligum.vladimir@gmail.com>
2022-09-06 08:42:35 +0300

    1st

---
+++ diceapp.py
@@ -0,0 +1,78 @@
+import random
+n = random.randint(1,6) 
+m = random.randint(1,6)
+if n == 1:
+    print("┌───────┐")
+    print("|       |")
+    print("|   ●   |")
+    print("|       |")
+    print("└───────┘")
+elif n == 2:
+    print("┌───────┐")
+    print("|     ● |")
+    print("|       |")
+    print("| ●     |")
+    print("└───────┘")
+elif n == 3:
+    print("┌───────┐")
+    print("|     ● |")
+    print("|   ●   |")
+    print("| ●     |")
+    print("└───────┘")
+elif n == 4:
+    print("┌───────┐")
+    print("| ●   ● |")
+    print("|       |")
+    print("| ●   ● |")
+    print("└───────┘")
+elif n == 5:
+    print("┌───────┐")
+    print("| ●   ● |")
+    print("|   ●   |")
+    print("| ●   ● |")
+    print("└───────┘")
+else:
+    print("┌───────┐")
+    print("| ●   ● |")
+    print("| ●   ● |")
+    print("| ●   ● |")
+    print("└───────┘")
+    
+    
+    
+if m == 1:
+    print("┌───────┐")
+    print("|       |")
+    print("|   ●   |")
+    print("|       |")
+    print("└───────┘")
+elif m == 2:
+    print("┌───────┐")
+    print("|     ● |")
+    print("|       |")
+    print("| ●     |")
+    print("└───────┘")
+elif m == 3:
+    print("┌───────┐")
+    print("|     ● |")
+    print("|   ●   |")
+    print("| ●     |")
+    print("└───────┘")
+elif m == 4:
+    print("┌───────┐")
+    print("| ●   ● |")
+    print("|       |")
+    print("| ●   ● |")
+    print("└───────┘")
+elif m == 5:
+    print("┌───────┐")
+    print("| ●   ● |")
+    print("|   ●   |")
+    print("| ●   ● |")
+    print("└───────┘")
+else:
+    print("┌───────┐")
+    print("| ●   ● |")
+    print("| ●   ● |")
+    print("| ●   ● |")
+    print("└───────┘")
