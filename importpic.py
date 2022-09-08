d7e3d0d191959dbf223ee6772033a3ffbc9b8eaa
vova.ligum <ligum.vladimir@gmail.com>
2022-09-08 08:52:49 +0300

    "VV'

--- importpic.py
+++ importpic.py
@@ -1,14 +1,12 @@
   
 import os
-os.mkdir('C:/test')
+os.mkdir('C:/vova')
 from importlib.resources import path
 import urllib.request
 
-
 URL = 'https://img.haarets.co.il/img/1.4062586/1504506551.jpg'
-FILENAME = r'c:\test\image1.jpg'
+FILENAME = r'c:\vova\image1.jpg'
 with urllib.request.urlopen(URL) as response:
     image = response.read()
     with open(FILENAME, 'wb') as output_file:
         output_file.write(image)
-
